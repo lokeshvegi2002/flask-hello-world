04f28ae970c70eb26acb43eb1779c86af8b61dfa
lokeshvegi2002 <lokeshvegi2002@gmail.com>
2025-06-10 06:38:40 +0530

    change made

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! ram'
+    return 'Hello, World! ram sitha'
 
 if __name__ == '__main__':
     app.run(debug=True)
